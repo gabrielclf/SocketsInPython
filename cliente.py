307251c6067608ff7e445d78ca28204257d57a3e
gabrielclf <42468620+gabrielclf@users.noreply.github.com>
2022-05-23 15:18:22 -0300

    Add files via upload
    Arquivos .py de modelos de cliente e servidor pra sockets em python

---
+++ cliente.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import socket
+HOST = '127.0.0.1'
+PORT = 50000
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+s.sendall(str.encode('Sucesso'))
+data = s.recv(1024)
+
+print('Mensagem enviada: ', data.decode())
