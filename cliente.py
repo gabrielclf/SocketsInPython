1b339862a0222b01b811d87d6f7d4792a1a8d937
gabrielclf <42468620+gabrielclf@users.noreply.github.com>
2022-05-24 12:45:51 -0300

    Update cliente.py
    Porta Definida

--- cliente.py
+++ cliente.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import socket
 HOST = '127.0.0.1'
-PORT = 50000
+PORT = 11102
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 s.sendall(str.encode('Sucesso'))
